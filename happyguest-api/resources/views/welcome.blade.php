111935e8caefcd5eaa71f3f2d7f9057d8a44dbc2
Diogo Mendes <diiogomendes21@hotmail.com>
2023-06-05 14:26:09 +0100

    Update WelcomePage

--- happyguest-api/resources/views/welcome.blade.php
+++ happyguest-api/resources/views/welcome.blade.php
@@ -828,16 +828,9 @@
 
 <body class="antialiased">
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-dots-lighter bg-gray-900 selection:bg-red-500 selection:text-white">
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
-
             <img src="{{ asset('logotipo.png') }}" alt="Logotipo" class="mx-auto">
-
-            <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
-                <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                </div>
-            </div>
         </div>
     </div>
 </body>
