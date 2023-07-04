5ad6be0cac555a46fb4b0f0444db3dbecfb759e6
Diogo Mendes <diiogomendes21@hotmail.com>
2023-07-04 16:46:19 +0100

    Update ItemRequest.php

--- happyguest-api/app/Http/Requests/ItemRequest.php
+++ happyguest-api/app/Http/Requests/ItemRequest.php
@@ -26,14 +26,15 @@ class ItemRequest extends FormRequest
             return [
                 'name' => 'string|min:5|max:255',
                 'nameEN' => 'string|min:5|max:255',
-                'price' => 'numeric|min:0|max:999999.99',
+                'price' => 'nullable|numeric|min:0|max:999999.99',
                 'category' => 'in:room,bathroom,drink,breakfast,lunch,dinner,snack,other',
-                'stock' => 'numeric|min:0|max:999999',
+                'stock' => 'nullable|numeric|min:0|max:999999',
             ];
         }
         // Store request
         return [
             'name' => 'required|string|min:5|max:255',
+            'nameEN' => 'required|string|min:5|max:255',
             'price' => 'nullable|numeric|min:0|max:999999.99',
             'type' => 'required|in:O,F', // O - Object, F - Food
             'category' => 'required|in:room,bathroom,drink,breakfast,lunch,dinner,snack,other',
