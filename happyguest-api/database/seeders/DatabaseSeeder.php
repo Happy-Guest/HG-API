ce7de6ae6db28d51db1b9f4a09a10bd510da91ea
Diogo Mendes <diiogomendes21@hotmail.com>
2023-07-27 20:11:50 +0100

    Menus Seed

--- happyguest-api/database/seeders/DatabaseSeeder.php
+++ happyguest-api/database/seeders/DatabaseSeeder.php
@@ -33,11 +33,25 @@ class DatabaseSeeder extends Seeder
         // Delete all files from public storage and storage
         $files = glob(storage_path('app/public/user_photos/*'));
         $files = array_merge($files, glob(storage_path('app/complaint_files/*')));
+        $files = array_merge($files, glob(storage_path('app/public/services/*')));
         foreach ($files as $file) {
             if (is_file($file)) {
                 unlink($file);
             } else {
                 File::deleteDirectory($file);
+            }
+        }
+
+        // Create services folder
+        if (!is_dir(storage_path('app/public/services'))) {
+            mkdir(storage_path('app/public/services'));
+        }
+
+        // Populate services folder from storage
+        $services = scandir(storage_path('app/services'));
+        foreach ($services as $service) {
+            if ($service !== '.' && $service !== '..') {
+                copy(storage_path('app/services/' . $service), storage_path('app/public/services/' . $service));
             }
         }
     }
